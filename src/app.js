22356de0017cdd88d101d95a19a46c7f472ccc3e
Eduardo-rico <emrs94@gmail.com>
2020-09-21 23:46:56 -0500

    jugando con rutas

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 import express from 'express';
-import morgan from 'morgan'
+import morgan from 'morgan';
 const app = express();
 
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
+//variables de express
+// app.set("nombre", vlor)
 
+//aca van las rutas
+app.get('/', (req, res) => {
+  console.log('hola');
+  res.json({
+    autor: 'Eduardo Rioc',
+    descripcion: ''
+  });
+});
 
 export default app;
